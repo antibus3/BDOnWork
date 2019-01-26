61771d81e5302f77f49fe654d081a8725b89a4ac
antibus3 <42910875+antibus3@users.noreply.github.com>
2019-01-26 21:39:32 +0500

    Antibus
    Добавил класс элемента чувствительного со всеми полями и конструктор его. Числовым полям, которых как бы нет, будут присваиваться 0. Если делать строки, то на мой взгляд не очень корректно будет.

--- BDOnWorkLib/BDOnWorkLib/SensitiveElement.cs
+++ BDOnWorkLib/BDOnWorkLib/SensitiveElement.cs
@@ -7,12 +7,62 @@ namespace BDOnWorkLib
 {
     public class SensitiveElement
     {
-        private string _NumberVK { get; set; }           
-             
-        public SensitiveElement ()
-        {
+        //  Основные характеристики Элемента Чуствительного
+        public readonly string NumberVK;            
+        public readonly string NumberSIOM;          
+        public readonly float SignalLeftSIOM;       
+        public readonly float SignalRigthSIOM;
+        public readonly float SPILeftSIOM;
+        public readonly float SPIRigthSIOM;
+        public readonly float LengthLeftSIOM;
+        public readonly float LengthRigthSIOM;
+        public readonly float SignalVK;
+        public readonly float SPIVK;
+        public readonly float LengthLeftVK;
+        public readonly float LengthRigthVK;
+        public readonly float ConstantSignal;
 
-        }
+        //  Побочные характеристики
+        public readonly string NumberTemperatureSensor;
+        public readonly bool IsExperement;
+                
+             
+        public SensitiveElement (
+            string numberVK,
+            string numberSIOM,
+            float signalLeftSIOM,
+            float signalRigthSIOM,
+            float sPILeftSIOM,
+            float sPIRigthSIOM,
+            float lengthLeftSIOM,
+            float lengthRigthSIOM,
+            float signalVK,
+            float sPIVK,
+            float lengthLeftVK,
+            float lengthRigthVK,
+            float constantSignal,
+            string numberTemperatureSensor,
+            bool isExperement
+                                )
+        {
+            NumberVK = numberVK;
+            NumberSIOM = numberSIOM;
+            SignalLeftSIOM = signalLeftSIOM;
+            SignalRigthSIOM = signalRigthSIOM;
+            SPILeftSIOM = sPILeftSIOM;
+            SPIRigthSIOM = sPIRigthSIOM;
+            LengthLeftSIOM = lengthLeftSIOM;
+            LengthRigthSIOM = lengthRigthSIOM;
+            SignalVK = signalVK;
+            SPIVK = sPIVK;
+            LengthLeftVK = lengthLeftVK;
+            LengthRigthVK = lengthRigthVK;
+            ConstantSignal = constantSignal;
+            NumberTemperatureSensor = numberTemperatureSensor;
+            IsExperement = isExperement;
+         }
+
+
 
     }
 }
